ae0fadb6c74f2fc92b0d7c4f28f3580293bc4dfe
thisLucio <luxusnietzsche@gmail.com>
2024-09-21 13:51:39 -0300

    Setup new repo

---
+++ src/test/java/com/thislucio/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thislucio.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
